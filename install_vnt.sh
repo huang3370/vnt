c2ebe75f8facef41bcf0f07a23e444e2b4ed4f6c
huang3370 <165783634+huang3370@users.noreply.github.com>
2025-02-18 15:37:20 +0800

    install_vnt.sh

--- install_vnt.sh
+++ install_vnt.sh
@@ -1,9 +1,16 @@
+
 #!/bin/bash
 
 # 下载并解压VNT客户端
 wget https://github.com/vnt-dev/vnt/releases/download/1.2.16/vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz
 tar -zxvf vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz
 cd vnt-x86_64-unknown-linux-musl-1.2.16
+
+# 提示用户输入自定义参数
+echo "直接输入组网号："
+read id
+echo "输入-s空格IP:端口使用官方服务器直接回车下一步："
+read fwq
 
 # 创建一个服务文件
 cat <<EOF > /etc/systemd/system/vnt-cli.service
@@ -13,7 +20,8 @@ After=network.target
 
 [Service]
 Type=simple
-ExecStart=/root/vnt-cli -k 12345678 --cmd
+ExecStart=/root/vnt-cli -k $id $fwq --cmd
+
 Restart=always
 User=root
 Group=root
