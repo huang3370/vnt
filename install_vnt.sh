1f68cf92f733e2e1d81e0092b3e8876a65cdd9b1
huang3370 <165783634+huang3370@users.noreply.github.com>
2025-02-24 05:21:36 +0800

    install_vnt.sh

--- install_vnt.sh
+++ install_vnt.sh
@@ -1,10 +1,23 @@
-
 #!/bin/bash
 
-# 下载并解压VNT客户端
-wget https://github.com/vnt-dev/vnt/releases/download/1.2.16/vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz
-tar -zxvf vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz
-cd vnt-x86_64-unknown-linux-musl-1.2.16
+# 定义文件名
+FILE_NAME="vnt-x86_64-unknown-linux-musl-1.2.16.tar.gz"
+DIRECTORY_NAME="vnt-x86_64-unknown-linux-musl-1.2.16"
+
+# 检查文件是否已存在
+if [ -f "$FILE_NAME" ]; then
+    echo "$FILE_NAME 已存在，跳过下载步骤。"
+else
+    # 下载并解压VNT客户端
+    wget https://github.com/vnt-dev/vnt/releases/download/1.2.16/$FILE_NAME
+fi
+
+# 检查解压后的目录是否存在
+if [ ! -d "$DIRECTORY_NAME" ]; then
+    tar -zxvf $FILE_NAME
+fi
+
+cd $DIRECTORY_NAME
 
 # 提示用户输入自定义参数
 echo "直接输入组网号："
